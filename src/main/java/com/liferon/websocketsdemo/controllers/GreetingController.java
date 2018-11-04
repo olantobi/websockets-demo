7ff003dd6403e6ffc953b8f577594449d25a9b30
Ebenezer Olanrewaju <ebenezer.olanrewaju@venturegardengroup.com>
2018-11-04 19:12:33 +0100

    Initial setup for demo app

---
+++ src/main/java/com/liferon/websocketsdemo/controllers/GreetingController.java
@@ -0,0 +1,19 @@
+package com.liferon.websocketsdemo.controllers;
+
+import com.liferon.websocketsdemo.messages.Greeting;
+import com.liferon.websocketsdemo.messages.HelloMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+@Controller
+public class GreetingController {
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception{
+        Thread.sleep(1000); // Simulated delay
+        return new Greeting("Hello, "+HtmlUtils.htmlEscape(message.getName())+"!");
+    }
+}
